d7dafcc5a1274045c248c012c4075dcaa475415a
Daniel Chrobak <danielchrobak12@gmail.com>
2024-09-21 06:08:33 -0500

    Update install.py

--- install.py
+++ install.py
@@ -1,9 +1,26 @@
 import os
 import subprocess
+import sys
+
+def install_required_packages():
+    required_packages = [
+        'customtkinter',
+        'requests',
+        'pystray',
+        'Pillow',
+        'pybip39',
+        'PyInstaller'
+    ]
+
+    for package in required_packages:
+        try:
+            __import__(package)
+        except ImportError:
+            print(f"Installing {package}...")
+            subprocess.check_call([sys.executable, '-m', 'pip', 'install', package])
 
 def build_executable():
     current_dir = os.path.dirname(os.path.realpath(__file__))
-
     icon_path = os.path.join(current_dir, "erwin.ico")
     script_path = os.path.join(current_dir, "erwin.py")
     output_dir = os.path.join(os.path.expanduser("~"), "Desktop")
@@ -15,26 +32,13 @@ def build_executable():
     os.makedirs(output_dir, exist_ok=True)
 
     command = [
-        "python", "-m", "PyInstaller",
+        sys.executable, "-m", "PyInstaller",
         "--add-data", f"{icon_path};.",
         "--noconsole",
         "--name=Erwin",
         f"--icon={icon_path}",
         f"--distpath={output_dir}",
         "--onedir",
-        "--hidden-import=customtkinter",
-        "--hidden-import=requests",
-        "--hidden-import=json",
-        "--hidden-import=random",
-        "--hidden-import=time",
-        "--hidden-import=threading",
-        "--hidden-import=tkinter",
-        "--hidden-import=tkinter.filedialog",
-        "--hidden-import=tkinter.messagebox",
-        "--hidden-import=pystray",
-        "--hidden-import=PIL",
-        "--hidden-import=PIL.Image",
-        "--hidden-import=pybip39",
         script_path
     ]
 
@@ -48,4 +52,5 @@ def build_executable():
         print(f"An unexpected error occurred: {e}")
 
 if __name__ == "__main__":
+    install_required_packages()
     build_executable()
